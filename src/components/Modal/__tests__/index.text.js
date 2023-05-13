33c8f84e486b942e8efe79182ada437e201b8630
cesarsiguencia <cesar.siguencia@gmail.com>
2023-05-13 15:58:31 -0400

    added modal test

---
+++ src/components/Modal/__tests__/index.text.js
@@ -0,0 +1,43 @@
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+const mockToggleModal = jest.fn();
+const currentPhoto = {
+  name: 'Park bench',
+  category: 'landscape',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+  i: 1
+};
+
+afterEach(cleanup);
+
+describe('Modal component', () => {
+  it('renders', () => {
+    render(<Modal
+      toggleModal={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
+  })
+
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Modal
+      toggleModal={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+describe('Click Event', () => {
+  it('calls toggleModal handler', () => {
+    const { getByText } = render(<Modal
+      toggleModal={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
+    fireEvent.click(getByText('Close this modal'))
+
+    expect(mockToggleModal).toHaveBeenCalledTimes(1);
+  });
+})  
